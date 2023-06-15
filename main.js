65d21d8530cca818a874120e3375c94226f4c2b3
Mah <madavetto@gmail.com>
2023-06-15 18:33:07 -0300

    modified: main.js

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-document.getElementById("#producao").innerHTML='Mad'
+//document.getElementById("#producao").innerHTML='Mad'
+console.log('Gustavo Pergola ama Mádjan, Mádjan ama Gustavo Pergola')
+
+
+
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", () => { // arrow function, funcao de seta, funcao vazia
+    console.log("Cliquei no robo")
+})
+
+// isso aqui é um bloco de código
+function dizOi(nome) {
+    console.log(nome)
+    console.log("Gustavo lindo!!!")
+    console.log("todo console funciona")
+}
+
+dizOi("Mad")  //para chamar a funcao assim que a pagina abre, é a primeira coisa que aparece
 
