6585e3ccc78a789e854138d946d8ef0c0388fde8
Mah <madavetto@gmail.com>
2023-06-16 18:24:56 -0300

    modified: main.js

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 //document.getElementById("#producao").innerHTML='Mad'
-console.log('Gustavo Pergola ama Mádjan, Mádjan ama Gustavo Pergola')
+//console.log('Gustavo Pergola ama Mádjan, Mádjan ama Gustavo Pergola')
 
 
 
-const robotron = document.querySelector("#robotron")
+//const robotron = document.querySelector("#robotron")
 
+
+/*
 robotron.addEventListener("click", (evento) => { // arrow function, funcao de seta, funcao vazia
-    console.log(evento)
+    //console.log(evento)
 })
 
 // isso aqui é um bloco de código
 function dizOi(nome) {
     console.log("oi " + nome)
     console.log("Gustavo lindo!!!")
-    console.log("todo console funciona")
 }
 
 dizOi("Mad")  //para chamar a funcao assim que a pagina abre, é a primeira coisa que aparece
+*/
+
+
+
+
+
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+somar.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) +1
+})
+
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) -1
+})
 
